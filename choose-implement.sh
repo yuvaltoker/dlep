07a61bdf0917a7c04929ea20fbb90ea7f2431110
YuvalToker <yuval@gmail.com>
2021-02-02 11:28:46 +0200

    fixed the bug when creating a modem and whether or not choosing a discovery iface, it would choose the default one

--- choose-implement.sh
+++ choose-implement.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$IMPLEMENTATION" = "modem" ]; then
-    /adjusted-dlep/build/Dlep config-file /adjusted-dlep/config/modem.xml
+    /adjusted-dlep/build/Dlep config-file /adjusted-dlep/config/modem.xml discovery-iface $DISCOVERY_IFACE
 elif [ "$IMPLEMENTATION" = "router" ]; then
     /adjusted-dlep/build/Dlep config-file /adjusted-dlep/config/router.xml discovery-iface $DISCOVERY_IFACE
 fi
