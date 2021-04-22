8202e9f186cf73ccc09899dce0239c45ff78d88d
yuvaltoker <yuvalt2710@gmail.com>
2021-04-22 17:14:54 +0300

    doesnt work yet, it is change the file as supposed in container, but not in /usr/local...

--- choose-implement.sh
+++ choose-implement.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # adjusting dlep-draft-8175.xml by the extensions from enviroment variables:
-sed -i "/^modem_name=/c\modem_name='$container_name'" ./modem/dock_modem/modem.py
 
-# /usr/local/etc/dlep/dlep-rfc-8175.xml - where the file will be eventually
-# adjusted-dlep/config/protocol/dlep-rfc-8175.xml - where the file in the container
-# adjusted-LL-DLEP/adjusted-DLEP/config/protocol/dlep-rfc-8175.xml - where the file in host
+# bla bla/usr/local/etc/dlep/dlep-rfc-8175.xml - where the file will be eventually
+# bla bla adjusted-dlep/config/protocol/dlep-rfc-8175.xml - where the file in the container
+# bla bla adjusted-LL-DLEP/adjusted-DLEP/config/protocol/dlep-rfc-8175.xml - where the file in host
 
-if [ "$EXT_PAUSE" = "true" ]
-	sed -i "/<!-- <xi:include href=\"pause-extension-8651.xml\"\/> -->/c\    <xi:include href=\"pause-extension-8651.xml\"/>" ./adjusted-dlep/config/protocol/dlep-rfc-8175.xml
+#if [ "$EXT_PAUSE" = "true" ]; then
+sed -i "/<!-- <xi:include href=\"pause-extension-8651.xml\"\/> -->/c\    <xi:include href=\"pause-extension-8651.xml\"/>" ./adjusted-dlep/config/protocol/dlep-rfc-8175.xml
+#fi
 
-if [ "$EXT_LID" = "true" ]
+if [ "$EXT_LID" = "true" ]; then
 	sed -i "/<!-- <xi:include href=\"lid-extension-8703.xml.xml\"\/> -->/c\    <xi:include href=\"lid-extension-8703.xml.xml\"/>" ./adjusted-dlep/config/protocol/dlep-rfc-8175.xml
-
+fi
 
 command_string="/adjusted-dlep/build/Dlep config-file /adjusted-dlep/config/"
 
@@ -39,5 +39,3 @@ fi
 
 echo "$command_string"
 eval "$command_string"
-
-
